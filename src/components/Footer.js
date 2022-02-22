ee9c8c0a7d2fc8687e723ad02e7ae829d26d313c
Karthik Shankar <umashankarg@cod.edu>
2022-02-22 09:27:24 -0600

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; 2021</p>
+            <a href='/about' style={{ color: 'white' }}>About</a>
+        </footer>
+    )
+}
+
+export default Footer
